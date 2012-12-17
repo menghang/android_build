252a14fe020e8fa7741c4c854171ad841c7505ad
martincz <martincz@mokeetek.com>
2012-12-17 13:19:04 +0800

    change dependies file name
    Change-Id: I9526e4b0fdb7577d153c69845fefe88cdc7c96f4

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -135,7 +135,7 @@ def add_to_manifest(repositories):
 
         print 'Adding dependency: MoKee/%s -> %s' % (repo_name, repo_target)
         project = ElementTree.Element("project", attrib = { "path": repo_target,
-            "remote": "github", "name": "MoKee/%s" % repo_name, "revision": "jb_mkt" })
+            "remote": "mokee", "name": "MoKee/%s" % repo_name, "revision": "jb_mkt" })
 
         if 'branch' in repository:
             project.set('revision',repository['branch'])
