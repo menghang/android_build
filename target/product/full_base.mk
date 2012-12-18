e35cf795a1ca2231522512c227718a24cd42e194
chenleicpp <chenleicpp@gmail.com>
2012-12-18 21:10:30 +0800

    Remove VideoEditor and VoiceDialer in others
    Change-Id: Ifa859a4054b1829603b28ab1b33468f44ea6b99c

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -24,15 +24,14 @@ PRODUCT_PACKAGES := \
     libdrmframework \
     libdrmframework_jni \
     libfwdlockengine \
-    VideoEditor \
     WAPPushManager
 
-PRODUCT_PACKAGES += \
-    libvideoeditor_jni \
-    libvideoeditor_core \
-    libvideoeditor_osal \
-    libvideoeditor_videofilters \
-    libvideoeditorplayer
+#PRODUCT_PACKAGES += \
+#    libvideoeditor_jni \
+#    libvideoeditor_core \
+#    libvideoeditor_osal \
+#    libvideoeditor_videofilters \
+#    libvideoeditorplayer
 
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
